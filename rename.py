8217389f3c7e5baa1d5415e776f76c6a24b1988d
nekomi2 <dore123222@gmail.com>
2021-04-05 17:59:26 -0400

    changed sort order

--- rename.py
+++ rename.py
@@ -1,23 +1,21 @@
 import os
 import sys
-'''renames files, files in format number_pnumber'''
+
 idx = 0
 src = os.getcwd()
 def rename(s):
     if(s[-6:-4] == 'p0'):
        global idx
        idx = idx + 1
-
-    return str(idx) + s[s.find('_'):]
+    prefix = '' if idx >= 10 else '0'
+    return prefix + str(idx) + s[s.find('_'):]
 
 
 print('renaming')
 script_name = sys.argv[0]
 filenames = os.listdir()
 filenames.remove(script_name)
-filenames.sort()
-
-
+filenames.sort(key=lambda x:int(x[:x.find('_')]),reverse=True)
 new_names = list(map(rename,filenames))
 for i in range(len(filenames)):
     os.rename(filenames[i],new_names[i])
