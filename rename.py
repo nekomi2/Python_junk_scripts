3f01d66aa7fb38598a62ed84ee726e271ddc71d8
nekomi2 <dore123222@gmail.com>
2021-04-15 18:28:04 -0400

    natural sort added

--- rename.py
+++ rename.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+from natsort import os_sorted
 idx = 0
 src = os.getcwd()
 def rename(s):
@@ -15,7 +15,8 @@ print('renaming')
 script_name = sys.argv[0]
 filenames = os.listdir()
 filenames.remove(script_name)
-filenames.sort(key=lambda x:int(x[:x.find('_')]),reverse=True)
+filenames = os_sorted(filenames) #natural sort
+filenames.sort(key=lambda x:int(x[:x.find('_')]),reverse=True) #Guaranteed to be stable
 new_names = list(map(rename,filenames))
 for i in range(len(filenames)):
     os.rename(filenames[i],new_names[i])
